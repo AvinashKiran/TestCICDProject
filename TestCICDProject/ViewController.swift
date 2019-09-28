e828e0fa29b4ce20c84bce98e7e47460eec05be4
avinash kiran <avinashkiran.teegela@carrier.com>
2019-09-28 11:38:49 +0530

    test commit

---
+++ TestCICDProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCICDProject
+//
+//  Created by HRDC on 28/09/19.
+//  Copyright © 2019 HRDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestCICDProjectTests/TestCICDProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestCICDProjectTests.swift
+//  TestCICDProjectTests
+//
+//  Created by HRDC on 28/09/19.
+//  Copyright © 2019 HRDC. All rights reserved.
+//
+
+import XCTest
+@testable import TestCICDProject
+
+class TestCICDProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
